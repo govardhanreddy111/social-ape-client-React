39ce97f6fe297eb868f22fd1acf49ef9a9ed10fb
Govardhan Reddy <bobbiligovardhanreddy@gmail.com>
2019-08-09 10:45:48 +0530

    Like & Unlike screams

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -3,11 +3,19 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { likeScream, unLikeScream} from "../redux/actions/dataActions";
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import MyButton from '../util/MyButton';
+import ChatIcon from '@material-ui/icons/Chat';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+
 
 const styles = {
     card : {
@@ -23,9 +31,42 @@ const styles = {
     }
 }
 class Scream extends Component{
+    likedScream = () =>{
+        if(this.props.user.likes && this.props.user.likes.find(like => like.screamId === this.props.scream.screamId)){
+            return true;
+        }else{
+            return false;
+        }
+    };
+    likeScream = () =>{
+        this.props.likeScream(this.props.scream.screamId);
+    };
+    unLikeScream = () =>{
+        this.props.unLikeScream(this.props.scream.screamId);
+    };
     render() {
         dayjs.extend(relativeTime);
-        const { classes, scream : { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount} } = this.props;
+        const { classes, scream : { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount},
+        user : {
+            authenticated
+        }} = this.props;
+        const likeButton = !authenticated ? (
+            <MyButton tip="Like">
+                <Link to="/login">
+                    <FavoriteBorder color="primary"/>
+                </Link>
+            </MyButton>
+        ) : (
+            this.likedScream() ? (
+                <MyButton tip="Undo like" onClick={this.unLikeScream}>
+                    <FavoriteIcon color="primary"/>
+                </MyButton>
+            ) : (
+                <MyButton tip="Like" onClick={this.likeScream}>
+                    <FavoriteBorder color="primary"/>
+                </MyButton>
+            )
+        );
         return(
             <Card className={classes.card}>
                 <CardMedia
@@ -35,10 +76,34 @@ class Scream extends Component{
                         <Typography variant="h5" color="primary" component={Link} to={`/users/${userHandle}`}>{userHandle}</Typography>
                         <Typography variant="body2" color="textSecondary">{dayjs(createdAt).fromNow()}</Typography>
                         <Typography variant="body1">{body}</Typography>
+                        {likeButton}
+                        <span>{likeCount} Likes</span>
+                        <MyButton tip="comments">
+                            <ChatIcon color="primary" />
+                        </MyButton>
+                        <span>{commentCount} Comments</span>
                     </CardContent>
             </Card>
         )
     }
 }
 
-export default withStyles(styles)(Scream);
+Scream.propTypes = {
+    likeScream : PropTypes.func.isRequired,
+    unLikeScream : PropTypes.func.isRequired,
+    user : PropTypes.object.isRequired,
+    scream : PropTypes.object.isRequired,
+    classes : PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state =>({
+    user : state.user
+});
+
+const mapStateToActions = {
+    likeScream,
+    unLikeScream
+};
+
+
+export default connect(mapStateToProps, mapStateToActions)(withStyles(styles)(Scream));
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,23 +5,19 @@ import Grid from '@material-ui/core/Grid'
 
 import Scream from '../components/Scream';
 import Profile from '../components/Profile';
+import PropTypes  from 'prop-types'
+
+import { connect } from 'react-redux';
+import { getScreams } from "../redux/actions/dataActions";
 class Home extends Component{
-    state = {
-        screams : null
-    }
     componentDidMount() {
-        axios.get('/screams')
-            .then(res =>{
-                this.setState({screams : res.data})
-            })
-            .catch(err =>{
-                console.log(err);
-            })
+        this.props.getScreams();
     }
 
     render() {
-        let recentScreamsMarkup = this.state.screams ? (
-            this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
+        const { screams, loading } = this.props.data;
+        let recentScreamsMarkup = !loading ? (
+            screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
         ) : <p>Loading...</p>
         return(
             <Grid container spacing={10}>
@@ -35,5 +31,13 @@ class Home extends Component{
         )
     }
 }
+Home.propTypes = {
+    getScreams : PropTypes.func.isRequired,
+    data : PropTypes.object.isRequired
+};
 
-export default Home;
+const mapStateToProps = state => ({
+    data : state.data
+});
+
+export default connect(mapStateToProps, {getScreams})(Home);
